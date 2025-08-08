ab54fbbcd3f1c7c5ca36b296430d3b7f99ce7859
wangshilong <wslzdsa@163.com>
2025-08-08 15:17:55 +0800

    chore: 更新项目路径并重新生成编译文件

--- InventoryERP.Infrastructure/obj/Debug/net8.0/InventoryERP.Infrastructure.AssemblyInfo.cs
+++ InventoryERP.Infrastructure/obj/Debug/net8.0/InventoryERP.Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("InventoryERP.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f1b9ca33e1c5d51557886bb06882aed7034ee00b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5eafe20de1cbefbe8fa47d229db387bdb6e543ab")]
 [assembly: System.Reflection.AssemblyProductAttribute("InventoryERP.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("InventoryERP.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
